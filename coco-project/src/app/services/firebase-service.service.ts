491f7969fcd3af38f10d2370aaf79e57aa5bb22b
mislavwinkler <mislav.winkler@gmail.com>
2025-09-11 00:58:03 +0200

    storageBucket rename
    Account for firebase changing naming scheme for storage buckets

--- coco-project/src/app/services/firebase-service.service.ts
+++ coco-project/src/app/services/firebase-service.service.ts
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAOYowA6Kjw1-Ss2e0otw--B9Rwa8w6XlI",
   authDomain: "coco-2024.firebaseapp.com",
   projectId: "coco-2024",
-  storageBucket: "coco-2024.appspot.com",
+  storageBucket: "coco-2024.firebasestorage.app",
   messagingSenderId: "614050471182",
   appId: "1:614050471182:web:759cf6910659c25e260b5f",
   measurementId: "G-V602CC9FVQ"
